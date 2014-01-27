11d96b47ec64a99efe6d68194c82a88eccf4c676
girinovey@girino.org <girinovey@girino.org>
2014-01-27 18:43:31 -0200

    V7 to V9 + donation (based on dga's)

--- src/sha_utils.h
+++ src/sha_utils.h
@@ -25,6 +25,16 @@ extern "C" {
 #include "sph_sha2.h"
 }
 
+#define SWAP64(n) \
+            (((n) << 56)                      \
+          | (((n) & 0xff00) << 40)            \
+          | (((n) & 0xff0000) << 24)          \
+          | (((n) & 0xff000000) << 8)         \
+          | (((n) >> 8) & 0xff000000)         \
+          | (((n) >> 24) & 0xff0000)          \
+          | (((n) >> 40) & 0xff00)            \
+          | ((n) >> 56))
+
 void sha512_func_fips(unsigned char* in, unsigned int size, unsigned char* out);
 void sha512_func_avx(unsigned char* in, unsigned int size, unsigned char* out);
 void sha512_func_sph(unsigned char* in, unsigned int size, unsigned char* out);
